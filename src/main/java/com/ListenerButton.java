37d8b917dbcb02a036cdba6b40995c3701a5d041
Draxler7 <109177949+Draxler7@users.noreply.github.com>
2023-10-19 21:54:30 +0500

    Проверка на пустые поля

--- src/main/java/com/ListenerButton.java
+++ src/main/java/com/ListenerButton.java
@@ -16,7 +16,12 @@ public class ListenerButton implements ActionListener {
         JTextField textField2 = main.window.getJTextField2();
         JTextField textField3 = main.window.getJTextField3();
         JTextField textField4 = main.window.getJTextField4();
-        if (true) {
+        String regex = "[0-9]+";
+        boolean check = textField.getText().matches(regex);
+        boolean check2 = textField2.getText().matches(regex);
+        boolean check3 = textField3.getText().matches(regex);
+        boolean check4 = textField4.getText().matches(regex);
+        if ((check == true) && (check2 == true) && (check3 == true) && (check4 == true)) {
             heatSolver heat = new heatSolver(Double.parseDouble(textField4.getText()),
                     Double.parseDouble(textField3.getText()), Double.parseDouble(textField2.getText()),
                     Double.parseDouble(textField.getText()));
@@ -26,6 +31,10 @@ public class ListenerButton implements ActionListener {
         } else {
             JOptionPane.showMessageDialog(main.window.getJFrame(),
                     "Введите корректные числа");
+            System.out.println(check);
+            System.out.println(check2);
+            System.out.println(check3);
+            System.out.println(check4);
         }
     }
 
