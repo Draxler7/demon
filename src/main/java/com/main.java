9b0711006352ff41bf1894d4ff3a41973286308c
Draxler7 <109177949+Draxler7@users.noreply.github.com>
2023-12-05 01:09:29 +0500

    add //main
    Change-Id: I24e2f9e4c814cf6f1250d6eb180d52f486388078

--- src/main/java/com/main.java
+++ src/main/java/com/main.java
@@ -3,7 +3,8 @@ package com;
 public class main {
 
     protected static Calculator window;
-
+//zd
+    // main class
     public static void main(String[] args) {
         window = new Calculator();
 
